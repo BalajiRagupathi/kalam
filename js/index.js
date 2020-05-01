c19457a39d541dd5550580898f4e96a30a1e2e2f
BalajiRagupathi <43380898+BalajiRagupathi@users.noreply.github.com>
2020-05-01 10:45:38 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,55 +1,12 @@
-var menu = document.querySelector('.nav__list');
-var burger = document.querySelector('.burger');
-var doc = $(document);
-var l = $('.scrolly');
-var panel = $('.panel');
-var vh = $(window).height();
-
-var openMenu = function() {
-  burger.classList.toggle('burger--active');
-  menu.classList.toggle('nav__list--active');
-};
-
-// reveal content of first panel by default
-panel.eq(0).find('.panel__content').addClass('panel__content--active');
-
-var scrollFx = function() {
-  var ds = doc.scrollTop();
-  var of = vh / 4;
-  
-  // if the panel is in the viewport, reveal the content, if not, hide it.
-  for (var i = 0; i < panel.length; i++) {
-    if (panel.eq(i).offset().top < ds+of) {
-     panel
-       .eq(i)
-       .find('.panel__content')
-       .addClass('panel__content--active');
-    } else {
-      panel
-        .eq(i)
-        .find('.panel__content')
-        .removeClass('panel__content--active')
+$(document).ready(function() {
+  $('.expandable-box').click(function(e) {
+    if($(this).hasClass('open')) {
+      $('.expandable-box.out').not($(this)).removeClass('out');
+      $(this).removeClass('open');
     }
-  }
-};
-
-var scrolly = function(e) {
-  e.preventDefault();
-  var target = this.hash;
-  var $target = $(target);
-
-  $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
-  }, 300, 'swing', function () {
-      window.location.hash = target;
+    else {
+      $('.expandable-box').not($(this)).addClass('out');
+      $(this).addClass('open');
+    }
   });
-}
-
-var init = function() {
-  burger.addEventListener('click', openMenu, false);
-  window.addEventListener('scroll', scrollFx, false);
-  window.addEventListener('load', scrollFx, false);
-  $('a[href^="#"]').on('click',scrolly);
-};
-
-doc.on('ready', init);
+});
